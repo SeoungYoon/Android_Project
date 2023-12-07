96556f8bc5542226c07cc8b88221c46a82c9015d
PC <tmddbs0326@naver.com>
2023-12-07 16:08:05 +0900

    good!

--- app/src/main/java/com/example/tlqkf/MainActivity.java
+++ app/src/main/java/com/example/tlqkf/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.tlqkf;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.TextView;
+import android.widget.Toast;
 
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+
+    SensorManager sensorManager;
+    Sensor stepCountSensor;
+    TextView stepCountView;
+
+    int currentSteps = 0;
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        stepCountView = findViewById(R.id.stepCountView);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION},0);
+            }
+        }
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+        if (stepCountSensor == null){
+            Toast.makeText(this, "No Step Sensor", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    public void onStart(){
+        super.onStart();
+        if(stepCountSensor != null){
+            sensorManager.registerListener(this,stepCountSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event){
+        if(event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR){
+            if(event.values[0]==1.0f){
+                currentSteps++;
+                stepCountView.setText(String.valueOf(currentSteps));
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
     }
 }
